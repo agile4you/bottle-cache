7007746600dc03e9aa05d82c7c9aaf4857037601
PyB1l <vpapavasil@gmail.com>
2016-01-24 15:18:58 +0200

    Added setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='bottle-cache',
-    version='1.1',
+    version='1.1a',
     packages=['test', 'bottle_cache'],
     url='https://github.com/agile4you/bottle-cache',
     license='GLPv3',
