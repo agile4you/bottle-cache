cbe4be00a34fdc835b6e0fb4a3669f0fc55fc394
PyB1l <vpapavasil@gmail.com>
2016-01-24 15:14:48 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='bottle-cache',
+    version='1.1',
+    packages=['test', 'bottle_cache'],
+    url='https://github.com/agile4you/bottle-cache',
+    license='GLPv3',
+    author='Papavassiliou Vassilis',
+    author_email='vpapavasil@gmail.com',
+    description='Cache Plugins for bottle.py applications'
+)
